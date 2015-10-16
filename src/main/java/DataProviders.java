29a4af7ec50a5fe742704d3593379088fc34a4e7
saap <saap.net@gmail.com>
2015-10-16 21:00:25 +0300

    change test data rows

--- src/main/java/DataProviders.java
+++ src/main/java/DataProviders.java
@@ -13,6 +13,7 @@ public class DataProviders {
                 {71, true},
                 {13, true},
                 {16, false},
+                {156, false},
                 {17, true},
                 {19, true},
         };
