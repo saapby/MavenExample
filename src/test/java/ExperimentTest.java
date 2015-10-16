f2db8f145dff597cbf1d6e5a0fbe741a428ea472
saap <saap.net@gmail.com>
2015-10-16 20:33:10 +0300

    Added experiment test

---
+++ src/test/java/ExperimentTest.java
@@ -0,0 +1,13 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Admin on 16.10.2015.
+ */
+public class ExperimentTest {
+
+    @Test
+    public void experimentTest() {
+        Assert.assertTrue(true);
+    }
+}
